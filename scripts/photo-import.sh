37bbdfb04a29ec57c34998583778243dce649bfd
Jamie Eisenhart <jamie@jamieei.org>
2013-08-15 09:24:44 -0700

    Photo import stats

--- scripts/photo-import.sh
+++ scripts/photo-import.sh
@@ -60,17 +60,7 @@ N_COPIED=0
 echo Processing files... >&2
 for FILE in $(find $SRC -type f -regex '.*\(JPG\|RAF\)')
 do
-    $((N_FILES++))
-
-    # Check for a duplicate
-    HASH=$(sha1sum $FILE | cut -f 1 -d ' ')
-    if [ ${HASHES[$HASH]} ]; then
-        echo "Skipping duplicate file $FILE" >&2
-        continue
-    fi
-
-    # Save the hash
-    declare -A HASHES=( [$HASH]=$FILE )
+    N_FILES=$((N_FILES+1))
 
     # Get the file data
     EXIF=$(exiftool -EXIF:CreateDate -t -d '%y%m%d %H%M%S' $FILE | cut -f 2)
@@ -78,55 +68,77 @@ do
     echo $FILE -\> EXIF=$EXIF, BASE=$BASE >&2
 
     # Validate
-    if [ $(echo $EXIF | wc -w) -ne 2 ]; then
+    VALID=true
+
+    if [[ $(echo $EXIF | wc -w) -ne 2 ]]; then
         echo "Missing EXIF data, skipping" >&2
-        continue
+        VALID=false
     fi
 
-    if [ $(echo $BASE| wc -w) -ne 1 ]; then
+    if [[ $VALID && $(echo $BASE | wc -w) -ne 1 ]]; then
         echo "Error extracting BASE, skipping" >&2
-        continue
+        VALID=false
     fi
     
-    # Print "CREATE_DATE CREATE_TIME BASE FILE"
-    echo "$EXIF $BASE $FILE"
+    # Check for duplicates
+    HASH=$(sha1sum $FILE | cut -f 1 -d ' ')
+    if [[ $VALID && ${HASHES[$HASH]} ]]; then
+        echo "Skipping duplicate file $FILE" >&2
+        VALID=false
+    fi
+
+    # Save the hash
+    declare -A HASHES=( [$HASH]=$FILE )
+
+    # Print file data to pipe. Note that EXIF has multiple fields.
+    echo "$N_FILES $EXIF $BASE $FILE $VALID"
 done | sort | while read LINE
 do
     # Parse the sorted line
-    CREATE_DATE=$(echo $LINE | cut -d ' ' -f 1)
-    BASE=$(echo $LINE | cut -d ' ' -f 3)
-    SRC_PATH=$(echo $LINE | cut -d ' ' -f 4)
+    N_FILES=$(echo $LINE | cut -d ' ' -f 1)
+    CREATE_DATE=$(echo $LINE | cut -d ' ' -f 2)
+    BASE=$(echo $LINE | cut -d ' ' -f 4)
+    SRC_PATH=$(echo $LINE | cut -d ' ' -f 5)
+    VALID=$(echo $LINE | cut -d ' ' -f 6)
 
-    # Create the destination directory
-    DEST_DIR=$DEST/$CREATE_DATE
-    if [ ! -d "$DEST_DIR" ]; then
-        evalOrSimulate "mkdir -pv $DEST_DIR"
+    if [[ $N_FILES == "1" ]]; then
+        echo Copying files... >&2
     fi
 
-    # Reset the sequence # for each destination directory
-    if [ "$DEST_DIR" != "$LASTDIR" ]; then
-        SEQ=0
-        LASTBASE=
+    if [[ $VALID  == "true" ]]; then
+        # Create the destination directory
+        DEST_DIR=$DEST/$CREATE_DATE
+        if [ ! -d "$DEST_DIR" ]; then
+            evalOrSimulate "mkdir -pv $DEST_DIR"
+        fi
+
+        # Reset the sequence # for each destination directory
+        if [ "$DEST_DIR" != "$LASTDIR" ]; then
+            SEQ=0
+            LASTBASE=
+        fi
+        LASTDIR=$DEST_DIR
+
+        # Increment the sequence number
+        if [ "$BASE" != "$LASTBASE" ]; then
+            SEQ=$((SEQ+1))
+            LASTBASE=$BASE
+        fi
+
+        # Compute the destination path
+        SRC_FILE=$(basename $SRC_PATH)
+        EXT=${SRC_FILE##$BASE}
+        DEST_PATH=$DEST_DIR/${CREATE_DATE}_$(printf '%04d' $SEQ)$EXT
+
+        # Copy the file
+        evalOrSimulate "cp -nv $SRC_PATH $DEST_PATH"
+        N_COPIED=$((N_COPIED+1))
     fi
-    LASTDIR=$DEST_DIR
 
-    # Increment the sequence number
-    if [ "$BASE" != "$LASTBASE" ]; then
-        SEQ=$((SEQ+1))
-        LASTBASE=$BASE
-    fi
+    # Print stats to escape them from the pipe subshell
+    echo "# files = $N_FILES"
+    echo "# copied = $N_COPIED"
+    echo "# skipped = $((N_FILES-N_COPIED))"
+done | tail -n 3 >&2
 
-    # Compute the destination path
-    SRC_FILE=$(basename $SRC_PATH)
-    EXT=${SRC_FILE##$BASE}
-    DEST_PATH=$DEST_DIR/${CREATE_DATE}_$(printf '%04d' $SEQ)$EXT
-
-    # Copy the file
-    evalOrSimulate "cp -nv $SRC_PATH $DEST_PATH"
-    N_COPIED=$((N_COPIED+1))
-done
-
-echo "# files = $N_FILES" >&2
-echo "# copied = $N_COPIED" >&2
-echo "# skipped = $((N_FILES-N_COPIED))" >&2
 
